61d895f0d48902931f8b55cc044bfd9cabd13876
Ed <efmerscher@gmail.com>
2025-01-12 19:38:07 +0000

    add database.js and .env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, responde) {
+import database from "../../../../infra/database.js";
+
+async function status(request, responde) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   responde.status(200).json({ chave: "valor" });
 }
 
